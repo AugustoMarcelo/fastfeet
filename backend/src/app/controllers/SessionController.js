06f74a51001acaf11fb58088958be835c5772d6e
AugustoMarcelo <mrclgst10@gmail.com>
2020-02-18 22:17:15 -0300

    Criado controller para gerenciar sessão do usuário

---
+++ backend/src/app/controllers/SessionController.js
@@ -0,0 +1,43 @@
+import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
+
+import authConfig from '../../config/auth';
+import User from '../models/User';
+
+class SessionController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(401).json({ error: 'Dados inválidos' });
+    }
+
+    const { email, password } = request.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return response.status(401).json({ error: 'Usuário não encontrado' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return response.status(401).json({ error: 'Senha incorreta' });
+    }
+
+    const { id, name } = user;
+
+    return response.json({
+      user: { id, name, email },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
