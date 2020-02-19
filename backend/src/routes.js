26fd57b8fbb5a7b928361e57f8fd92de137d3819
AugustoMarcelo <mrclgst10@gmail.com>
2020-02-18 22:15:24 -0300

    Criadas rotas para cadastrar, listar e atualizar destinatários

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (request, response) => response.json({ hello: 'world' }));
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.get('/recipients', RecipientController.index);
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
 
 export default routes;
