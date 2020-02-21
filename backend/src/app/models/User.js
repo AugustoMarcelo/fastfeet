0c5353e302b143b4971474d580736788b88615c3
AugustoMarcelo <mrclgst10@gmail.com>
2020-02-21 13:42:29 -0300

    Corrigindo senha para virtual

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -7,7 +7,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
-        password: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
