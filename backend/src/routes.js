c818a4639b202fbdea1b86fc26d8d74a2d9cb8a1
AugustoMarcelo <mrclgst10@gmail.com>
2020-02-21 13:38:23 -0300

    Criando rotas para entregadores e upload de arquivos

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliveryManController from './app/controllers/DeliveryManController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -13,5 +18,12 @@ routes.use(authMiddleware);
 routes.get('/recipients', RecipientController.index);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+routes.get('/deliverymen', DeliveryManController.index);
+routes.post('/deliverymen', DeliveryManController.store);
+routes.put('/deliverymen/:id', DeliveryManController.update);
+routes.delete('/deliverymen/:id', DeliveryManController.destroy);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
