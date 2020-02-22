3c546aff0013cf842b68056342ee557992df6e5b
AugustoMarcelo <mrclgst10@gmail.com>
2020-02-22 10:38:26 -0300

    Criando rotas para gerenciar encomendas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliveryManController from './app/controllers/DeliveryManController';
+import DeliveryController from './app/controllers/DeliveryController';
 import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
@@ -23,6 +24,11 @@ routes.get('/deliverymen', DeliveryManController.index);
 routes.post('/deliverymen', DeliveryManController.store);
 routes.put('/deliverymen/:id', DeliveryManController.update);
 routes.delete('/deliverymen/:id', DeliveryManController.destroy);
+
+routes.get('/deliveries', DeliveryController.index);
+routes.post('/deliveries', DeliveryController.store);
+routes.put('/deliveries/:id', DeliveryController.update);
+routes.delete('/deliveries/:id', DeliveryController.destroy);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
