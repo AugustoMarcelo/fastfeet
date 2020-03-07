f3d4940a1c58a4e7f525d7d7dadd5a25b9e6fe5d
AugustoMarcelo <mrclgst10@gmail.com>
2020-03-07 11:04:45 -0300

    Configurando eslint e estrutura de pastas do projeto

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    "prettier/prettier": ["error", { "endOfLine": "auto" }],
+    "react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".js"] }],
+    "import/prefer-default-export": "off",
+    "react/require-default-props": "off",
+    "no-console": ["error", { allow: ["tron"] }],
+    "no-param-reassign": "off",
+    "react/jsx-props-no-spreading": "off"
+  },
+};
