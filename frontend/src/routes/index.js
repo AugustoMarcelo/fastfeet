cf537e7cb0a34fc117d25c563cc015a8ff79663c
AugustoMarcelo <mrclgst10@gmail.com>
2020-03-12 16:14:04 -0300

    Adicionadas as páginas de listagem de destinatários e entregadores

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -4,12 +4,16 @@ import Route from './Route';
 
 import Login from '../pages/Login';
 import Deliveries from '../pages/Deliveries';
+import Deliveryman from '../pages/Deliveryman';
+import Recipients from '../pages/Recipients';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Login} />
       <Route path="/deliveries" component={Deliveries} isPrivate />
+      <Route path="/deliveryman" component={Deliveryman} isPrivate />
+      <Route path="/recipients" component={Recipients} isPrivate />
     </Switch>
   );
 }
