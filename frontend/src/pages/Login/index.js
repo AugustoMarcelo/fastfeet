31b4b225d6b8fa650fcc59880a2a4a4af1479a00
AugustoMarcelo <mrclgst10@gmail.com>
2020-03-13 13:50:35 -0300

    Adicionada validação do form e animação no botão

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import * as Yup from 'yup';
 import { Form } from '@unform/web';
+import { FaSpinner } from 'react-icons/fa';
 
 import Input from '../../components/Form/Input';
 
@@ -7,8 +9,32 @@ import logo from '../../assets/logo.png';
 import { Container, Card, Logo } from './styles';
 
 export default function Login() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const formRef = useRef(null);
+  const loading = false;
+
+  async function handleSubmit(data) {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email('E-mail inválido')
+          .required('E-mail é obrigatório'),
+        password: Yup.string().required('Senha é obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const errorMessages = {};
+
+        error.inner.forEach(err => {
+          errorMessages[err.path] = err.message;
+        });
+
+        formRef.current.setErrors(errorMessages);
+      }
+    }
   }
 
   return (
@@ -18,7 +44,7 @@ export default function Login() {
           <img src={logo} width={250} alt="FastFeet" />
         </Logo>
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <Input
             name="email"
             type="email"
@@ -31,7 +57,13 @@ export default function Login() {
             label="Senha:"
             placeholder="*******"
           />
-          <button type="submit">Entrar no sistema</button>
+          <button type="submit" disabled={loading ? 1 : 0}>
+            {loading ? (
+              <FaSpinner size={20} color="#fff" />
+            ) : (
+              'Entrar no sistema'
+            )}
+          </button>
         </Form>
       </Card>
     </Container>
