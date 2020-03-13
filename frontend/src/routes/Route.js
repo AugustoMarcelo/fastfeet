cac52f66a119778dd5ee926f654113d28124a157
AugustoMarcelo <mrclgst10@gmail.com>
2020-03-13 16:17:01 -0300

    Verificando se usuário está logado via store

--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import { store } from '../store';
+
 import DefaultLayout from '../pages/_layouts/default';
 
 export default function RouteWrapper({
@@ -9,7 +11,7 @@ export default function RouteWrapper({
   isPrivate,
   ...rest
 }) {
-  const signed = true;
+  const { signed } = store.getState().auth;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
