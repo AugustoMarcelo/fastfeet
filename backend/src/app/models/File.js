d4ad047f14ed558a7951eccc69905e7b49f0d1ae
AugustoMarcelo <mrclgst10@gmail.com>
2020-03-17 13:49:18 -0300

    Alterando url estática por variável ambiente

--- backend/src/app/models/File.js
+++ backend/src/app/models/File.js
@@ -9,7 +9,7 @@ class File extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return `localhost:3333/files/${this.path}`;
+            return `${process.env.APP_URL}/files/${this.path}`;
           },
         },
       },
