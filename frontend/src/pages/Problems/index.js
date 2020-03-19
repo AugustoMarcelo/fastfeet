59101e63ea0918f717a06b2443256ffdb2aff997
AugustoMarcelo <mrclgst10@gmail.com>
2020-03-19 17:15:11 -0300

    Criada página de problemas

---
+++ frontend/src/pages/Problems/index.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+
+import api from '~/services/api';
+
+import PageHeaderList from '~/components/PageHeaderList';
+import DropdownMenu from '~/components/Dropdown';
+import { EmptyContent } from '~/components/styles/Table';
+import { Table } from './styles';
+
+export default function Problems() {
+  const [problems, setProblems] = useState([]);
+  const [query, setQuery] = useState('');
+
+  async function loadProblems() {
+    const response = await api.get('deliveries/problems', {
+      params: {
+        q: query,
+      },
+    });
+    setProblems(response.data.rows);
+  }
+
+  useEffect(() => {
+    loadProblems();
+  }, []);
+
+  useEffect(() => {
+    loadProblems();
+  }, [query]);
+
+  function handleSearch(text) {
+    setQuery(text);
+  }
+
+  return (
+    <>
+      <PageHeaderList
+        pageTitle="Problemas na entrega"
+        inputPlaceholder="Buscar por encomendas"
+        handleSearch={handleSearch}
+      />
+      {problems.length ? (
+        <Table>
+          <thead>
+            <tr>
+              <th>Encomenda</th>
+              <th>Problema</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {problems.map(problem => (
+              <tr key={problem.id}>
+                <td>{`#${problem.delivery.id}`}</td>
+                <td>{problem.description}</td>
+                <td>
+                  <DropdownMenu
+                    onView={() => {}}
+                    onDelete={() => {}}
+                    deleteLabel="Cancelar encomenda"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <EmptyContent>Nenhuma encomenda com problemas</EmptyContent>
+      )}
+    </>
+  );
+}
