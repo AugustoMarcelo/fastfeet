26bd335e9851750d34f2e6a85d9e1960785cc3a9
AugustoMarcelo <mrclgst10@gmail.com>
2020-03-31 21:50:28 -0300

    Registrando sagas de entregadores

--- frontend/src/store/modules/rootSaga.js
+++ frontend/src/store/modules/rootSaga.js
@@ -2,7 +2,8 @@ import { all } from 'redux-saga/effects';
 
 import auth from './auth/sagas';
 import recipients from './recipients/sagas';
+import deliveryman from './deliveryman/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, recipients]);
+  return yield all([auth, recipients, deliveryman]);
 }
