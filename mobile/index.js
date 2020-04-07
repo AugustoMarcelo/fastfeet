10b43045579073f33d433cec1f89a776d8636d20
AugustoMarcelo <mrclgst10@gmail.com>
2020-04-07 14:51:19 -0300

    Ignorando mensagem de componente StepIndicator

--- mobile/index.js
+++ mobile/index.js
@@ -1,5 +1,10 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './src';
 import { name as appName } from './app.json';
+
+YellowBox.ignoreWarnings([
+  'Warning: componentWillReceiveProps',
+  'Animated: `useNativeDriver`',
+]);
 
 AppRegistry.registerComponent(appName, () => App);
