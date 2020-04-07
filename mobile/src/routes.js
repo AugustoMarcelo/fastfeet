b3eeb1e4a903ccbceb1445df804e52600cb0bac7
AugustoMarcelo <mrclgst10@gmail.com>
2020-04-06 22:15:33 -0300

    Finalizado arquivo com todas as rotas

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,15 +1,110 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Login from './pages/Login';
-import Deliveries from './pages/Deliveries';
+import Dashboard from './pages/Dashboard';
+import DeliveryDetails from './pages/DeliveryDetails';
+import RegisterProblem from './pages/RegisterProblem';
+import ConfirmDelivery from './pages/ConfirmDelivery';
+import Problems from './pages/Problems';
 import Profile from './pages/Profile';
 
 const Stack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
+
+function deliveriesStack({ navigation }) {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: '#fff',
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          fontSize: 16,
+          fontWeight: 'bold',
+        },
+        headerLeftContainerStyle: {
+          marginLeft: 10,
+          alignItems: 'center',
+        },
+        headerStyle: {
+          backgroundColor: '#7D40E7',
+          elevation: 0,
+        },
+      }}
+    >
+      <Stack.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={DeliveryDetails}
+        options={{
+          title: 'Detalhes da encomenda',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.navigate('Dashboard')}>
+              <Icon name="keyboard-arrow-left" size={28} color="#fff" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="RegisterProblem"
+        component={RegisterProblem}
+        options={{
+          title: 'Informar problema',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.navigate('Details')}>
+              <Icon name="keyboard-arrow-left" size={28} color="#fff" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Problems"
+        component={Problems}
+        options={{
+          title: 'Visualizar problemas',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.navigate('Details')}>
+              <Icon name="keyboard-arrow-left" size={28} color="#fff" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="ConfirmDelivery"
+        component={ConfirmDelivery}
+        options={{
+          title: 'Confirmar entrega',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.navigate('Details')}>
+              <Icon name="keyboard-arrow-left" size={28} color="#fff" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+deliveriesStack.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }),
+};
+
+function tabBarIcon(color, iconName) {
+  return <Icon name={iconName} color={color} size={26} />;
+}
 
 export default function createRouter(isSigned = false) {
   return !isSigned ? (
@@ -39,12 +134,10 @@ export default function createRouter(isSigned = false) {
       >
         <Tabs.Screen
           name="Deliveries"
-          component={Deliveries}
+          component={deliveriesStack}
           options={{
             tabBarLabel: 'Entregas',
-            tabBarIcon: ({ color }) => (
-              <Icon name="reorder" size={26} color={color} />
-            ),
+            tabBarIcon: ({ color }) => tabBarIcon(color, 'reorder'),
           }}
         />
         <Tabs.Screen
@@ -52,9 +145,7 @@ export default function createRouter(isSigned = false) {
           component={Profile}
           options={{
             tabBarLabel: 'Meu Perfil',
-            tabBarIcon: ({ color }) => (
-              <Icon name="account-circle" size={26} color={color} />
-            ),
+            tabBarIcon: ({ color }) => tabBarIcon(color, 'account-circle'),
           }}
         />
       </Tabs.Navigator>
