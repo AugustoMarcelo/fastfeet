671d4e0e5a42f3ee0724771ea420e54f9bfa1367
AugustoMarcelo <mrclgst10@gmail.com>
2020-04-09 20:59:27 -0300

    Implementado cadastro de problema para uma entrega

--- mobile/src/pages/RegisterProblem/index.js
+++ mobile/src/pages/RegisterProblem/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar, ToastAndroid } from 'react-native';
 import PropTypes from 'prop-types';
+
+import api from '../../services/api';
 
 import {
   Container,
@@ -13,7 +15,26 @@ import {
   ButtonSendText,
 } from './styles';
 
-export default function DeliveryDetails({ navigation }) {
+export default function RegisterProblem({ navigation, route }) {
+  const { id } = route.params;
+  const [description, setDescription] = useState('');
+
+  async function handleRegisterProblem() {
+    const { status } = await api.post(`delivery/${id}/problems`, {
+      description,
+    });
+
+    if (status === 201) {
+      ToastAndroid.show('Problema cadastrado', ToastAndroid.LONG);
+      navigation.goBack();
+    } else {
+      ToastAndroid.show(
+        'Não foi possível cadastrar o problema',
+        ToastAndroid.LONG
+      );
+    }
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
@@ -26,9 +47,11 @@ export default function DeliveryDetails({ navigation }) {
                 placeholder="Inclua aqui o problema que ocorreu na entrega."
                 numberOfLines={15}
                 textAlignVertical="top"
+                value={description}
+                onChangeText={setDescription}
               />
             </Card>
-            <ButtonSend onPress={() => navigation.goBack()}>
+            <ButtonSend onPress={handleRegisterProblem}>
               <ButtonSendText>Enviar</ButtonSendText>
             </ButtonSend>
           </ContentOverlap>
@@ -38,9 +61,14 @@ export default function DeliveryDetails({ navigation }) {
   );
 }
 
-DeliveryDetails.propTypes = {
+RegisterProblem.propTypes = {
   navigation: PropTypes.shape({
     navigate: PropTypes.func.isRequired,
     goBack: PropTypes.func.isRequired,
+  }),
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
   }),
 };
